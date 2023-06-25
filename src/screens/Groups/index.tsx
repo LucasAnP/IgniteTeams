e0bc620b43c996f2a121152cf1adcf1bb4334d36
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-06-25 18:52:55 -0300

    CONFIG: Add styled components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Container, Title } from "./styles";
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
