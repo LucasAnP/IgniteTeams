3d8ae9eb39c80d89bead125f6e272a6d8da58564
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-06-26 20:35:01 -0300

    CONFIG: Use fonts added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { Groups } from "@screens/Groups";
-import theme from "@theme/index";
 import { ThemeProvider } from "styled-components/native";
+import { ActivityIndicator } from "react-native";
+import { Groups } from "@screens/Groups";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import theme from "@theme/index";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
