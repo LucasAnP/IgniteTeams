e1798ea3f02ead00f351a1fdb0b236aef6f4e3c1
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-06-29 10:33:06 -0300

    Navigation: GoBack to home when header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImg from "@assets/logo.png";
 
@@ -5,10 +6,16 @@ type Props = {
   showBackButton?: boolean;
 };
 export const Header = ({ showBackButton = false }: Props) => {
+  const navigation = useNavigation();
+
+  const handleGoHome = () => {
+    navigation.navigate("groups");
+  };
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoHome}>
           <BackIcon />
         </BackButton>
       )}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,14 @@ import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 
 export const NewGroup = () => {
+  const navigation = useNavigation();
+  const handleNew = () => {
+    navigation.navigate("players", { group: "Rocket" });
+  };
+
   return (
     <Container>
       <Header showBackButton />
@@ -14,7 +20,7 @@ export const NewGroup = () => {
 
         <Highlight title="New team" subtitle="Create a team to add people" />
         <Input placeholder="Team name" />
-        <Button title="Create" style={{ marginTop: 20 }} />
+        <Button title="Create" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
